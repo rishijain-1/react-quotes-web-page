18d3c046095f94b16473634ad0ae115d8ac16d64
rishijain2002 <rishijain0652@gmail.com>
2024-08-12 12:32:18 +0530

    Create Quotes structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Quote from "./Quote"
+function App(){
+  return <>
+    
+    <Quote/>
+  </>
+
+}
+
+export default App
---
+++ src/Quote.jsx
@@ -0,0 +1,21 @@
+import React,{useEffect, useState} from 'react';
+import './Quote.css'
+
+const Quote=()=>{
+ 
+
+    
+    return(
+      
+        
+        <div className="container">
+            <div className="content">
+                <p>Quaotes</p>
+                <p className="author">-Author</p>
+            </div>
+            <div className="button">click me</div>
+        </div>
+    )
+}
+
+export default Quote;
