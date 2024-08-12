e5415e6788cb051a74e1002d769412163421c8b2
rishijain2002 <rishijain0652@gmail.com>
2024-08-12 13:13:50 +0530

    Add Loading fucn

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -2,35 +2,33 @@ import React,{useEffect, useState} from 'react';
 import './Quote.css'
 
 const Quote=()=>{
- 
     const Quotes=[];
     const [quote,setquote]= useState({
-        text:"Quote of the Day",
-        author:"Author",
+        text:"Be your own Developer",
+        author:"Rishi Jain",
     });
-   
+    const [loading,setLoading] = useState(false)
 
-
- const getQuote=async()=>{
-        
+    const getQuote=async()=>{
+        setLoading(true);
         const url="https://type.fit/api/quotes";
         const response=await fetch(url);
         const Quotes=await response.json();
-        console.log(Quotes);
         const num=Math.floor(Math.random()*Quotes.length);
         setTimeout(() => {
             setquote(Quotes[num]);
+            setLoading(false); // Set loading to false when fetching ends
         }, 1000);
     }
-
     
     return(
       
         
         <div className="container">
             <div className="content">
+                {loading && <div className="loader">Loading....</div>}
                 <p>{quote.text}</p>
-                <p className="author">-{quites.author.split(',')[0]}</p>
+                <p className="author">-{quote.author.split(',')[0]}</p>
             </div>
             <div className="button" onClick={()=>{getQuote()}}>click me</div>
         </div>
