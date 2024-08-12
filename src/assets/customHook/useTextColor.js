0cde475895d16528bb7e1d322035cf61f573c743
rishijain2002 <rishijain0652@gmail.com>
2024-08-12 15:56:48 +0530

    Add useTextColor Custom hook

---
+++ src/assets/customHook/useTextColor.js
@@ -0,0 +1,11 @@
+import {useState} from "react";
+const useTextColor = (initialColor="black") =>{
+    const [color,setColor] = useState[initialColor];
+    const changeColor =(newColor)=>{
+        setColor(newColor);
+    };
+
+    return [color,changeColor];
+};
+
+export default useTextColor;
